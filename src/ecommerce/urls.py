1fff70d4a159de460259923449235a6a64058438
EversonMagalhaes <everslay@gmail.com>
2025-12-02 01:53:05 -0300

    formulario de contato optimizado com django

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -21,6 +21,6 @@ from .views import home_page , about_page, contact_page
 urlpatterns = [
     path('', home_page),
     path('about', about_page),
-    path('contact', contact_page),
+    path('contact', contact_page, name='contact'),
     path('admin/', admin.site.urls),
 ]
--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,5 +1,7 @@
+from django.contrib import messages
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import ContactForm
 def home_page(request):
     context = {
         "title": "Página principal",
@@ -15,10 +17,24 @@ def about_page(request):
     return render(request, "about/view.html", context)
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
         "title": "Página de contato",
-        "content": "Bem-vindo a página de contato"
+        "content": "Bem-vindo a página de contato",
+        'form': contact_form
     }
     if request.method == 'POST':
         print(request.POST)
+
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+        # 1. SALVAR OS DADOS AQUI (ou enviar email, etc.)
+            
+        # 2. ADICIONAR A MENSAGEM DE SUCESSO
+        messages.success(request, 'Sua mensagem foi enviada com sucesso!')
+            
+        # 3. FAZER O REDIRECIONAMENTO (LIMPA O FORMULÁRIO!)
+        # Substitua 'nome_da_url_de_contato' pelo nome que você deu a essa view no seu urls.py
+        return redirect('contact')
+
     return render(request, "contact/view.html", context)
