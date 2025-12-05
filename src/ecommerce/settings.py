6775c683b002f6e293b2cbe872e3283cd37435e8
EversonMagalhaes <everslay@gmail.com>
2025-12-04 22:54:51 -0300

    insert MEDIA_URL e MEDIA_ROOT in settings.py

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -124,6 +124,9 @@ os.path.join(BASE_DIR, "static_local")
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
