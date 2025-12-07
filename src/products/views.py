cc8259bff0a9c9c115dad859928e072df3fb9e73
EversonMagalhaes <everslay@gmail.com>
2025-12-06 22:17:10 -0300

    Refactor(views): Remove unused and obsolete product views
    Remove código de Views de produto que não serão mais utilizadas ou que foram substituídas por versões mais robustas.
    
    Motivo:
    Comenta as funções e classes que demonstravam diferentes abordagens para listagem e detalhe de produtos (FBVs, CBVs por PK, e CBVs de Featured), mantendo apenas as Views finais (ProductListView e ProductDetailSlugView) que usam a lógica de slug.
    
    Isso limpa o código e remove ruído visual do arquivo `views.py`.

--- src/products/views.py
+++ src/products/views.py
@@ -1,18 +1,18 @@
 from django.http import Http404
 from django.views.generic import ListView, DetailView
-from django.shortcuts import render, get_object_or_404
+#from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
-class ProductFeaturedListView(ListView):
-    template_name = "products/list.html"
+# class ProductFeaturedListView(ListView):
+#     template_name = "products/list.html"
     
-    def get_queryset(self, *args, **kwargs):
-        return Product.objects.featured()
+#     def get_queryset(self, *args, **kwargs):
+#         return Product.objects.featured()
 
-class ProductFeaturedDetailView(DetailView):
-    queryset = Product.objects.all().featured()
-    template_name = "products/featured-detail.html"
+# class ProductFeaturedDetailView(DetailView):
+#     queryset = Product.objects.all().featured()
+#     template_name = "products/featured-detail.html"
 
 #Class Based View
 class ProductListView(ListView):
@@ -42,54 +42,54 @@ class ProductDetailSlugView(DetailView):
         return instance
 
 
-#Function Based View
-def product_list_view(request):
-    queryset = Product.objects.all()
-    context = {
-        'qs': queryset,
-        'object_list': queryset
-    }
-    return render(request, "products/list.html", context)
+# #Function Based View
+# def product_list_view(request):
+#     queryset = Product.objects.all()
+#     context = {
+#         'qs': queryset,
+#         'object_list': queryset
+#     }
+#     return render(request, "products/list.html", context)
 
-#Class Based View
-class ProductDetailView(DetailView):
-    #traz todos os produtos do banco de dados sem filtrar nada 
-    #queryset = Product.objects.all() 
-    template_name = "products/detail.html"
+# #Class Based View
+# class ProductDetailView(DetailView):
+#     #traz todos os produtos do banco de dados sem filtrar nada 
+#     #queryset = Product.objects.all() 
+#     template_name = "products/detail.html"
     
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
-    #     print(context)
-    #     return context
-    def get_object(self, *args, **kwargs):
-        pk = self.kwargs.get('pk')
-        instance = Product.objects.get_by_id(pk)
-        if instance is None:
-            raise Http404("Esse produto não existe!")
-        return instance
+#     # def get_context_data(self, *args, **kwargs):
+#     #     context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+#     #     print(context)
+#     #     return context
+#     def get_object(self, *args, **kwargs):
+#         pk = self.kwargs.get('pk')
+#         instance = Product.objects.get_by_id(pk)
+#         if instance is None:
+#             raise Http404("Esse produto não existe!")
+#         return instance
 
-#Function Based View
-def product_detail_view(request, pk = None, *args, **kwargs):
-    #instance = get_object_or_404(Product, pk = pk) #← maneira mais simples de testar
-    # maneira 1 de testar
-    # try:
-    #     instance = Product.objects.get(id = pk)
-    # except Product.DoesNotExist:
-    #     print("Nenhum produto encontrado aqui!")
-    #     raise Http404("Esse produto não existe!")
+# #Function Based View
+# def product_detail_view(request, pk = None, *args, **kwargs):
+#     #instance = get_object_or_404(Product, pk = pk) #← maneira mais simples de testar
+#     # maneira 1 de testar
+#     # try:
+#     #     instance = Product.objects.get(id = pk)
+#     # except Product.DoesNotExist:
+#     #     print("Nenhum produto encontrado aqui!")
+#     #     raise Http404("Esse produto não existe!")
     
-    #qs = Product.objects.filter(id = pk) # ← maneira 2 e 3 de testar
-    #if qs.exists(): ← maneira 2 de testar
-    # if qs.count() == 1: #← maneira 3 de testar
-    #     instance = qs.first()
-    # else:
-    #     raise Http404("Esse produto não existe!")
-    instance = Product.objects.get_by_id(pk)
-    print(instance)
-    if instance is None:
-        raise Http404("Esse produto não existe!")
+#     #qs = Product.objects.filter(id = pk) # ← maneira 2 e 3 de testar
+#     #if qs.exists(): ← maneira 2 de testar
+#     # if qs.count() == 1: #← maneira 3 de testar
+#     #     instance = qs.first()
+#     # else:
+#     #     raise Http404("Esse produto não existe!")
+#     instance = Product.objects.get_by_id(pk)
+#     print(instance)
+#     if instance is None:
+#         raise Http404("Esse produto não existe!")
     
-    context = {
-        'object': instance
-    }
-    return render(request, "products/detail.html", context)
+#     context = {
+#         'object': instance
+#     }
+#     return render(request, "products/detail.html", context)
